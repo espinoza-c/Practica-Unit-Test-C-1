4ec1799a4000d79e4eeba72409ba8a3993e8a97a
espinoza-c <carlos-antonio.espinoza@atos.net>
2022-07-27 11:39:16 -0500

    First Commit

---
+++ xUnitTests/CalculatorShould.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculators;
+using Xunit;
+
+namespace xUnitTests
+{
+    public class CalculatorShould
+    {
+        [Fact]
+        public void Add()
+        {
+
+            //Arrange
+            Calculator addValidator = new Calculator();
+            float numeroUno = 5.01f;
+            float numeroDos = 6.03f;
+            float expected = numeroDos + numeroUno;
+
+            //Add
+            float isEqual = addValidator.Add(numeroUno, numeroDos);
+
+            //Assert
+            Assert.Equal(expected, isEqual);
+        }
+        [Fact]
+        public void Sub()
+        {
+            //Arrange
+            Calculator addValidator = new Calculator();
+            float numeroUno = 5.01f;
+            float numeroDos = 6.03f;
+            float expected = numeroUno -  numeroDos;
+
+            //Add
+            float isEqual = addValidator.Sub(numeroUno, numeroDos);
+
+            //Assert
+            Assert.Equal(expected, isEqual);
+        }
+
+        [Theory]
+        [InlineData(10.1,10.2)]
+        [InlineData(5.2, 3.5)]
+        [InlineData(8000.1,2)]
+        [InlineData(850, 10.2)]
+        [InlineData(3.1416, 5.555)]
+
+        public void ValidateMul(float a, float b)
+        {
+            //Arrange
+            Calculator mulValidator = new Calculator();
+
+            //Add
+            float result = mulValidator.Mul(a, b);
+            float expected = a * b;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10.1, 10.2)]
+        [InlineData(5.2, 3.5)]
+        [InlineData(8000.1, 2)]
+        [InlineData(850, 10.2)]
+        [InlineData(3.1416, 5.555)]
+
+        public void ValidateDiv(float a, float b)
+        {
+            //Arrange
+            Calculator divValidator = new Calculator();
+
+            //Add
+            float result = divValidator.Div(a, b);
+            float expected = a / b;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
---
+++ xUnitTests/DivCero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculators;
+using Xunit;
+
+namespace xUnitTests
+{
+    public class DivCero
+    {
+        [Fact]
+        public void RaiseErrorWhenDividedByZero()
+        {
+            //Arrange
+            Calculator divZeroValidator = new Calculator();
+
+            //Act
+
+            //Assert
+            Assert.Throws<DivideByZeroException>(() => divZeroValidator.Div(10.1f, 0));
+        }
+    }
+}
